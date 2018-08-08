e4688983eaa611a29351a535610e0578753745b0
jliu666 <jliu666@hotmail.cm>
2018-08-08 22:59:00 +0800

    构建基本框架，采用fatjar方式

---
+++ skill6-parent/skill6-website/src/main/java/cn/skill6/website/Skill6WebsiteApplication.java
@@ -0,0 +1,12 @@
+package cn.skill6.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill6WebsiteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Skill6WebsiteApplication.class, args);
+  }
+}
