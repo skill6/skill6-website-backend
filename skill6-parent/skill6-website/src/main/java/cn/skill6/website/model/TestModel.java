885888312d822c8acf49b3c596008cba3428b376
jliu666 <jliu666@hotmail.com>
2018-08-14 00:08:17 +0800

    添加mybatis demo及配置

---
+++ skill6-parent/skill6-website/src/main/java/cn/skill6/website/model/TestModel.java
@@ -0,0 +1,24 @@
+package cn.skill6.website.model;
+
+public class TestModel {
+
+  private int id;
+
+  private int name;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getName() {
+    return name;
+  }
+
+  public void setName(int name) {
+    this.name = name;
+  }
+}
