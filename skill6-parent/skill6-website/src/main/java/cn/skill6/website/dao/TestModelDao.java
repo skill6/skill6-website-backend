10c798b54a64a311f93a67ab5f1826e9e6ef0783
jliu666 <jliu666@hotmail.com>
2018-08-16 23:30:08 +0800

    测试ci

--- skill6-parent/skill6-website/src/main/java/cn/skill6/website/dao/TestModelDao.java
+++ skill6-parent/skill6-website/src/main/java/cn/skill6/website/dao/TestModelDao.java
@@ -3,12 +3,6 @@ package cn.skill6.website.dao;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
-/**
- * 你好
- *
- * @desc 你好
- * @author liujichun
- */
 @Mapper
 public interface TestModelDao {
 
