267175dfca8eded99ac4b05bf098fa7a018db87b
jliu666 <jliu666@hotmail.com>
2018-08-16 23:21:38 +0800

    添加ci

--- skill6-parent/skill6-website/src/main/java/cn/skill6/website/dao/TestModelDao.java
+++ skill6-parent/skill6-website/src/main/java/cn/skill6/website/dao/TestModelDao.java
@@ -3,9 +3,19 @@ package cn.skill6.website.dao;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
-/** mybatis示例注解方式示例 */
+/**
+ * 你好
+ *
+ * @desc 你好
+ * @author liujichun
+ */
 @Mapper
 public interface TestModelDao {
+
+  /**
+   * @param id
+   * @return
+   */
   @Delete("DELETE test_model WHERE ID = #{id}")
   int deleteTestModelById(int id);
 }
