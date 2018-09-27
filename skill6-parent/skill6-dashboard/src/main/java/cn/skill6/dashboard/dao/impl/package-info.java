3980c430877d8d898348593b7ccd1c0aeb883815
何明胜 <husen@hemingsheng.cn>
2018-09-27 22:24:49 +0800

    1、增加相应的包;

---
+++ skill6-parent/skill6-dashboard/src/main/java/cn/skill6/dashboard/dao/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 数据库操作接口实现类，提供调用。在接口类名称后面加Oper(operate,操作)表示接口实现
+ *
+ * @author 何明胜
+ * @version 1.0.0
+ * @since 2018年9月27日 下午10:18:04
+ */
+package cn.skill6.dashboard.dao.impl;
---
+++ skill6-parent/skill6-dashboard/src/main/java/cn/skill6/dashboard/model/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 模型实体类
+ *
+ * @author 何明胜
+ * @version 1.0.0
+ * @since 2018年9月27日 下午10:23:27
+ */
+package cn.skill6.dashboard.model;
