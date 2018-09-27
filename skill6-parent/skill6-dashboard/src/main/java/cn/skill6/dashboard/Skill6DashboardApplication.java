0d8a999bcceac884e9d65c0a2b3a2e0c8c17fba1
何明胜 <husen@hemingsheng.cn>
2018-09-27 22:54:48 +0800

    1、增加main方法;

---
+++ skill6-parent/skill6-dashboard/src/main/java/cn/skill6/dashboard/Skill6DashboardApplication.java
@@ -0,0 +1,32 @@
+package cn.skill6.dashboard;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 测试启动类
+ *
+ * @author 何明胜
+ * @version 1.0.1
+ * @since 2018年8月21日 下午11:40:08
+ */
+@SpringBootApplication
+@ComponentScan("cn.skill6")
+@MapperScan(basePackages = {"cn.skill6.website.dao"})
+@EnableScheduling
+public class Skill6DashboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Skill6DashboardApplication.class, args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+}
