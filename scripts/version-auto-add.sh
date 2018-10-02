4acaf5e68e0579b1aa3eca44a2435d0c3ad96bd3
何明胜 <husen@hemingsheng.cn>
2018-10-02 20:14:05 +0800

    更新脚本

--- scripts/version-auto-add.sh
+++ scripts/version-auto-add.sh
@@ -4,13 +4,13 @@ set -ev
     #version add prepare
 	mvn install:install-file -DgroupId=cn.skill6.plugin -DartifactId=vautoadd-maven-plugin -Dversion=1.0.0 -Dpackaging=jar -Dfile=../plugins/vautoadd-maven-plugin-1.0.0.jar
 	#global setting
-	git config --global user.name "Travis Bot"
-	git config --global user.email "husen@hemingsheng.cn"
+	#git config --global user.name "Travis Bot"
+	#git config --global user.email "husen@hemingsheng.cn"
 	#push to remote
 	mvn vautoadd:vadd
 	git add .
 	git commit -m "Updated Version By One By Travis"
-	git push -u "https://12d96a4d5a7aadc74ccbdb7350e9091893526d78@github.com/skill6/skill6.git" master
+	git push -u origin master
 #fi
 
 mvn test -B
