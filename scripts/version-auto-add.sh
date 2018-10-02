dd262dac6bed585ca928431085ddca2a81d61dac
何明胜 <husen@hemingsheng.cn>
2018-10-02 20:51:33 +0800

    更新脚本

--- scripts/version-auto-add.sh
+++ scripts/version-auto-add.sh
@@ -6,7 +6,7 @@ set -ev
 	#global setting
 	git config --global user.name "Travis Bot"
 	git config --global user.email "husen@hemingsheng.cn"
-	ssh-keygen -t rsa -C "husen@hemingsheng.cn" | send "\r" | send "\r" | send "\r"
+	ssh-keygen -t rsa -f /home/travis/.ssh/id_rsa -q -N ""
 	cd .ssh
 	cat id_rsa.pub
 	ssh -T git@github.com
