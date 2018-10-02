cee12b2fea5ff6c1ee5a1201a67ddb47a0d14b6e
何明胜 <husen@hemingsheng.cn>
2018-10-02 16:54:30 +0800

    更新脚本

--- scripts/version-auto-add.sh
+++ scripts/version-auto-add.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo "hello world"
-  #version add prepare
-  mvn install:install-file -DgroupId=cn.skill6.plugin -DartifactId=vautoadd-maven-plugin -Dversion=1.0.0 -Dpackaging=jar -Dfile=plugins/vautoadd-maven-plugin-1.0.0.jar
-  #global setting
-  git config --global user.name "Travis Bot"
-  git config --global user.email "husen@hemingsheng.cn"
-  #push to remote
-  cd skill6-parent
-  mvn vautoadd:vadd
-  git add .
-  git commit -m "Updated Version By One By Travis"
-  git push -u origin master
+    #version add prepare
+	mvn install:install-file -DgroupId=cn.skill6.plugin -DartifactId=vautoadd-maven-plugin -Dversion=1.0.0 -Dpackaging=jar -Dfile=plugins/vautoadd-maven-plugin-1.0.0.jar
+	#global setting
+	git config --global user.name "Travis Bot"
+	git config --global user.email "husen@hemingsheng.cn"
+	#push to remote
+	cd skill6-parent
+	mvn vautoadd:vadd
+	git add .
+	git commit -m "Updated Version By One By Travis"
+	git push -u origin master
 fi
 
 mvn test -B
